e79330b9949df56028c42a117d7db47af77fefc3
lsamoylv <lsamoylv@exflmacliubov.desy.de>
2014-09-09 11:43:20 +0200

    try 2 to reduce FELsource_out

--- fast2h5_new_SP.py
+++ fast2h5_new_SP.py
@@ -470,7 +470,8 @@ def add_wf_attributes(fname0):
     wf_struct = Wavefront(wfr)
     if doPrint: print('Resizing and saving the wavefront data with attributes:'+bare_fname)
     #Resizing: decreasing Range of Horizontal and Vertical Position:
-    wpg.srwlib.srwl.ResizeElecField(wfr._srwl_wf, 'c', [0, 0.5, 1, 0.5,  1]);
+    from wpg.srwlib import srwl
+    srwl.ResizeElecField(wfr, 'c', [0, 0.5, 1, 0.5,  1]);
     wf_struct.store_hdf5(bare_fname)
     if doPrint: print('Replacing data with attributes from  '+bare_fname)
     with h5py.File(bare_fname) as h2:
